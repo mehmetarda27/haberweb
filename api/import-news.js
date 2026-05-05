207b997629e04fd70410f69eb0e362391615c975
Mehmet Arda Doğan <dersim3327@gmail.com>
2026-05-05 18:29:23 +0300

    api fix 🚀

--- api/import-news.js
+++ api/import-news.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
-return res.status(200).json({
-ok: true,
-route: "import-news",
-message: "Import news API route is working"
-});
+  res.status(200).json({
+    ok: true,
+    message: "API working 🚀",
+    route: "import-news"
+  });
 }
